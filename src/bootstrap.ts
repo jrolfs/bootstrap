abb7b8f0ba5d9473bb664b70da70dd925cdda661
Jamie Rolfs <jamie.rolfs@gmail.com>
2024-10-28 13:18:01 -0700

    fix: fix force on linking

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -124,7 +124,7 @@ const setupHomeshick = async () => {
   );
 
   console.log('Linking dotfiles...');
-  await shell('bash', ['-c', `${homeshick} link`, '--force']);
+  await shell('bash', ['-c', `${homeshick} link --force`]);
 };
 
 const bootstrap = async () => {
